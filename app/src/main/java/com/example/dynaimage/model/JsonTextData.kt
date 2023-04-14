2c453aa970be7d48c4b0faa9dedf768d267091d7
Vishal Rana <job.vishalrana@gmail.com>
2023-04-15 00:02:55 +0530

    completed text conversion for uploaded file

---
+++ app/src/main/java/com/example/dynaimage/model/JsonTextData.kt
@@ -0,0 +1,6 @@
+package com.example.dynaimage.model
+
+data class JsonTextData(
+    val language: String,
+    val word: String
+)
--- app/src/main/java/com/example/dynaimage/views/TextConvertFragment.kt
+++ app/src/main/java/com/example/dynaimage/views/TextConvertFragment.kt
@@ -1,21 +1,108 @@
 package com.example.dynaimage.views
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dynaimage.R
+import com.example.dynaimage.databinding.FragmentTextConvertBinding
+import com.example.dynaimage.model.JsonTextData
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.intellij.lang.annotations.Language
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class TextConvertFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var contentResolver: ContentResolver
+    private lateinit var binding: FragmentTextConvertBinding
+    // Request code for selecting a PDF document.
+    val PICK_FILE = 1
+    var fileDataObj : List<JsonTextData>? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_convert, container, false)
+        savedInstanceState: Bundle?): View {
+        binding = FragmentTextConvertBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        contentResolver = context?.contentResolver.let { it!! }
+
+        binding.btnUploadFile.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "*/*"
+
+                // Optionally, specify a URI for the file that should appear in the
+                // system file picker when it loads.
+                putExtra(DocumentsContract.EXTRA_INITIAL_URI, true)
+            }
+
+            startActivityForResult(intent, PICK_FILE)
+        }
+        binding.btnSpainish.setOnClickListener {
+            checkWordAsPerLanguage("spainish")
+        }
+        binding.btnHindi.setOnClickListener {
+            checkWordAsPerLanguage("hindi")
+        }
+        binding.btnIrish.setOnClickListener {
+            checkWordAsPerLanguage("Irish")
+        }
+        binding.btnRussian.setOnClickListener {
+            checkWordAsPerLanguage("Russian")
+        }
+        binding.btnTelugu.setOnClickListener {
+            checkWordAsPerLanguage("telugu")
+        }
+    }
+
+    private fun checkWordAsPerLanguage(lang: String) {
+        val data = fileDataObj?.find { lg->
+            lg.language.equals(lang, true)
+        }
+        binding.txtHelloWorld.text = data?.word ?: getString(R.string.str_hello_world)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == PICK_FILE
+            && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            binding.txtFileName.text = resultData?.data?.path
+            resultData?.data?.also { uri ->
+                val plainText = readTextFromUri(uri)
+                val jsonString = Gson().toJson(plainText)
+                fileDataObj = GsonBuilder().create().fromJson(plainText, Array<JsonTextData>::class.java).toList()
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun readTextFromUri(uri: Uri): String {
+        val stringBuilder = StringBuilder()
+        contentResolver.openInputStream(uri)?.use { inputStream ->
+            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                var line: String? = reader.readLine()
+                while (line != null) {
+                    stringBuilder.append(line)
+                    line = reader.readLine()
+                }
+            }
+        }
+        return stringBuilder.toString()
     }
 }
