6499d7d380260d44f1f2ea27f67f5a9e34bcb279
Vishal Rana <job.vishalrana@gmail.com>
2023-04-16 17:49:05 +0530

    added screen orintation logic

--- app/src/main/java/com/example/dynaimage/MainActivity.kt
+++ app/src/main/java/com/example/dynaimage/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.dynaimage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dynaimage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (!isTablet()) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        }
         // used nav controller for fragment navigation
         // val navHostFragment =
         //     supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -26,5 +30,11 @@ class MainActivity : AppCompatActivity() {
         //         }
         //     }
         // }
+    }
+
+    private fun isTablet(): Boolean {
+        return ((this.resources.configuration.screenLayout
+            and Configuration.SCREENLAYOUT_SIZE_MASK)
+            >= Configuration.SCREENLAYOUT_SIZE_LARGE)
     }
 }
